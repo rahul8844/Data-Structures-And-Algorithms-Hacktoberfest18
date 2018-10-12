0da9ed7ab86bd25f26de615fe1eeb297542834f1
Rahul Gupta <RLE0592@rlpl.com>
2018-10-12 18:53:07 +0530

    12th Oct Rahul8844

--- java/prime_number.java
+++ java/prime_number.java
@@ -1,17 +1,20 @@
+import java.lang.Math;
+import java.util.Scanner;
 public class Prime
 {    
     public static void main(String args[])
     {    
-        int i,m=0,flag=0;      
-        int n=3;//it is the number to be checked    
-        m=n/2;      
+		Scanner scn=new Scanner(System.in);
+        int flag=0;      
+		System.out.print(" Enter a number to be checked as a prime :: ");
+        int n=scn.nextInt();//it is the number to be checked         
         if(n==0||n==1)
         {  
           System.out.println(n+" is not prime number");      
         }
         else
         {  
-          for(i=2;i<=m;i++)
+          for(int i=2;i<=(int)Math.sqrt(n);i++)
           {      
             if(n%i==0)
             {      
